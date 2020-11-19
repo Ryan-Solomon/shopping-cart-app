155ebf31c76c8d648d1275ff62513796646f4880
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-18 19:49:29 -0600

    good enough styles for this

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,23 +1,24 @@
 import React, { FC } from 'react';
 import { TProduct } from '../../types/types';
+import './ProductItem.styles.css';
 
 type Props = {
   product: TProduct;
 };
 
 const ProductItem: FC<Props> = ({ product }) => {
-  const { title, price, description, category, image } = product;
+  const { title, price, description, image } = product;
 
   return (
     <div className='product-card'>
-      <img style={{ width: '100px' }} src={image} alt={title} />
+      <div
+        className='product-img'
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
       <div className='product-details'>
         <h2>{title}</h2>
         <p>{description}</p>
-      </div>
-      <div className='price-and-category'>
-        <h4>{price}</h4>
-        <h4>{category}</h4>
+        <h4>${price}</h4>
       </div>
     </div>
   );
